ca0b81c3225d9e93f9f15fda85dac5e0a1d485c7
PLCoster <58193305+PLCoster@users.noreply.github.com>
2022-07-15 16:17:25 -0700

    Updated webpack config the HTMLWebpackPlugin and webpack dev server for development

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,23 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  // Generate index.html from template file during build
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+    }),
+  ],
   // Export the production build to the dist folder, in the file main.js
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true, // Clean up the dist folder before each build
   },
   // Any SASS files are compiled to css, then into CommonJS
   // and then added as style nodes to the page (check build <head>)
