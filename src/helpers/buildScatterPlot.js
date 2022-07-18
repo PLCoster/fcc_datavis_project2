875cbba9fd95c96ab544258abb97bd6640f849f9
PLCoster <58193305+PLCoster@users.noreply.github.com>
2022-07-18 12:40:20 -0700

    Tooltips displaying data point info added to graph
    Tooltips are positioned based on the point location in the graph,
    in order to keep them visible in the graph area
    
    Style transitions added to grow / shrink and alter opacity on tooltip

--- src/helpers/buildScatterPlot.js
+++ src/helpers/buildScatterPlot.js
@@ -17,6 +17,9 @@ export default async function buildScatterPlot(raceData, width) {
   const height = 0.6 * width;
   const padding = 80;
 
+  const DOPED_COLOR = 'rgba(255,0,0,0.3)';
+  const CLEAN_COLOR = 'rgba(0,0,255,0.3)';
+
   const graphSVG = graphContainer
     .append('svg')
     .attr('class', 'graph')
@@ -31,8 +34,7 @@ export default async function buildScatterPlot(raceData, width) {
   const xscale = d3
     .scaleLinear()
     .domain([xMin - 1, xMax + 1])
-    .range([padding, width - padding])
-    .nice();
+    .range([padding, width - padding]);
 
   const yscale = d3
     .scaleLinear()
@@ -84,12 +86,79 @@ export default async function buildScatterPlot(raceData, width) {
     .data(raceData)
     .enter()
     .append('circle')
+    .attr('class', 'dot')
+    .attr('data-xvalue', (d) => d.Year)
+    .attr('data-yvalue', (d) => new Date(d.Seconds * 1000))
     .attr('cx', (d) => xscale(d.Year))
     .attr('cy', (d) => yscale(d.Seconds))
     .attr('r', 5)
-    .attr('fill', (d) =>
-      d.Doping ? 'rgba(255,0,0,0.5)' : 'rgba(0,0,255,0.5)'
-    );
+    .attr('fill', (d) => {
+      return d.Doping ? DOPED_COLOR : CLEAN_COLOR;
+    });
 
-  console.log('Made it here!!!!');
+  // Add on hover tool-tip to graph
+
+  const tooltip = graphContainer
+    .append('div')
+    .style('position', 'absolute')
+    .style('opacity', 0.0)
+    .attr('id', 'tooltip');
+
+  graphSVG
+    .selectAll('circle')
+    .data(raceData)
+    .on('mouseover', function (event, pointData) {
+      console.log('Mouse over!', pointData);
+
+      console.log('This is: ', this);
+
+      // Move tooltip to mouse position
+      tooltip
+        .html('')
+        .attr('data-year', pointData.Year)
+        .attr('data-time', pointData.Seconds)
+        .style('display', 'block')
+        .style('width', '200px')
+        .style('opacity', 1.0)
+        .style('background-color', pointData.Doping ? '#ffb2b2' : '#b2b2ff')
+        .style('top', `${event.layerY - 20}px`);
+
+      // Position tooltip to the left or right of the cursor depending on position
+      console.log(pointData.Year - xMin);
+      if (pointData.Year - xMin < (xMax - xMin) / 2) {
+        tooltip.style('left', `${event.layerX + 20}px`);
+      } else {
+        tooltip.style('left', `${event.layerX - 220}px`);
+      }
+
+      // Add datapoint text to tooltip
+      tooltip
+        .append('p')
+        .style('font-weight', 600)
+        .text(`${pointData.Name} (${pointData.Nationality})`);
+
+      tooltip
+        .append('p')
+        .text(`Year: ${pointData.Year},  Time: ${pointData.Time}`);
+
+      if (pointData.Doping) {
+        tooltip.append('p').attr('class', 'details').text(pointData.Doping);
+      }
+
+      // Highlight all datapoints for the same athlete
+      graphSVG
+        .selectAll('circle')
+        .data(raceData)
+        .attr('stroke', (d) => {
+          return d.Name === pointData.Name ? 'black' : 'none';
+        })
+        .attr('r', (d) => {
+          return d.Name === pointData.Name ? 10 : 5;
+        });
+    })
+    .on('mouseout', () => {
+      // Undo any mouseover effects
+      tooltip.style('opacity', 0.0).style('width', '0');
+      graphSVG.selectAll('circle').attr('stroke', 'none').attr('r', 5);
+    });
 }
